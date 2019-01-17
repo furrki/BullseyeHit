7dd34ce151e73af8d991fcea33747b5e72611f00
Admin <admin@Admins-Mac.local>
2019-01-17 20:10:03 +0300

    Initial Commit

--- BullseyeHit/Controller/MainVC.swift
+++ BullseyeHit/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  BullseyeHit
 //
 //  Created by Admin on 17.01.2019.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
